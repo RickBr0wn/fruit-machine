f8257895a0d925f72e1fec4ddf32aaee270b58c5
Rick Brown <ricky.brown.00@gmail.com>
2020-11-04 15:54:42 +0000

    Initial Commit

---
+++ Fruit Machine/Fruit_MachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fruit_MachineApp.swift
+//  Fruit Machine
+//
+//  Created by Rick Brown on 04/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct Fruit_MachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
